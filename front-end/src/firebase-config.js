cdf52bb8d5f66ab8a0e806ef5006c3dcc5a3ae5a
Abhinav-Iquadra <abhinav.avasarala@iquadra.com>
2025-06-17 16:40:29 -0400

    migrated to native firebase database
    migrated to native firebase database from google cloud console because the MongoDB-compatible Firestore (Enterprise Edition) does not integrate with Firebase Authentication or Firebase SDKs

--- front-end/src/firebase-config.js
+++ front-end/src/firebase-config.js
@@ -1,17 +1,19 @@
-// Firebase configuration and initialization
+// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPOQksNL51gXQKt9uZ-yR8g116GQUArPA",
-  authDomain: "bullseye-462519.firebaseapp.com",
-  projectId: "bullseye-462519",
-  storageBucket: "bullseye-462519.firebasestorage.app",
-  messagingSenderId: "568217572650",
-  appId: "1:568217572650:web:e5d74c238e91d2d683fa53",
-  measurementId: "G-Y724XQLP91"
+  apiKey: "AIzaSyC6RTUNZYBMgeZalTYZQ6hECQkt681rVjE",
+  authDomain: "bullseye-app-5c820.firebaseapp.com",
+  projectId: "bullseye-app-5c820",
+  storageBucket: "bullseye-app-5c820.firebasestorage.app",
+  messagingSenderId: "1050459949251",
+  appId: "1:1050459949251:web:b5cb87ada8279e5d91ba76",
+  measurementId: "G-25713K3XTF"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
